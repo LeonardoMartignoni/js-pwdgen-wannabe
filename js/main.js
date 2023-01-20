07f8cca6f9c5a6d705e61c6e2027c30930a1c99f
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-01-20 14:49:17 +0100

    Added Passw Generator

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Chiedo il nome
+let userName = prompt("Inserisci il tuo nome");
+// console.log("Nome: " + userName);
+
+// Chiedo il cognome
+let userSurname = prompt("Inserisci il tuo cognome");
+// console.log("Cognome: " + userSurname);
+
+// Chiedo il colore preferito
+let favouriteColor = prompt("Inserisci il tuo colore preferito");
+// console.log("Colore: " + favouriteColor);
+
+// Concateno le stringhe
+const passwordGenerated = userName + userSurname + favouriteColor;
+// console.log("Passw: " + passwordGenerated);
+
+// Inserisco la password generata nel paragrafo
+document.getElementById('generated_password').innerHTML = passwordGenerated;
